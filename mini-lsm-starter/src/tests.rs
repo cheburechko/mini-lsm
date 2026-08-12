7d64bffd2b10a52633fda94545f101b2eef9457a
Pavel Cherenkov <the.pavel.cherenkov@gmail.com>
2026-08-12 16:22:15 +0200

    3.1 Add test

--- mini-lsm-starter/src/tests.rs
+++ mini-lsm-starter/src/tests.rs
@@ -1,8 +1,8 @@
 //! DO NOT MODIFY -- Mini-LSM tests modules
 //! This file will be automatically rewritten by the copy-test command.
 
-mod harness;
 mod week1_day1;
+mod harness;
 mod week1_day2;
 mod week1_day3;
 mod week1_day4;
@@ -15,3 +15,4 @@ mod week2_day3;
 mod week2_day4;
 mod week2_day5;
 mod week2_day6;
+mod week3_day1;
