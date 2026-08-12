e0521efcc73cb7ee767fece6692f3c62936f8e34
Pavel Cherenkov <the.pavel.cherenkov@gmail.com>
2026-08-12 11:57:59 +0200

    Sync manifest on every write, fix file flags for recover

--- mini-lsm-starter/src/manifest.rs
+++ mini-lsm-starter/src/manifest.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fs::OpenOptions;
 use std::io::BufReader;
-use std::ops::Deref;
 use std::path::Path;
 use std::sync::Arc;
 use std::{fs::File, io::Write};
@@ -43,18 +43,20 @@ impl Manifest {
     }
 
     pub fn recover(path: impl AsRef<Path>) -> Result<(Self, Vec<ManifestRecord>)> {
-        let result = Self {
-            file: Arc::new(Mutex::new(File::open(path)?)),
-        };
         let mut manifest = Vec::new();
         {
-            let file = result.file.lock();
-            let mut deser = serde_json::Deserializer::from_reader(BufReader::new(file.deref()));
+            let file = File::open(&path)?;
+            let mut deser = serde_json::Deserializer::from_reader(BufReader::new(file));
             while let Ok(record) = ManifestRecord::deserialize(&mut deser) {
                 manifest.push(record);
             }
         }
-        Ok((result, manifest))
+        Ok((
+            Self {
+                file: Arc::new(Mutex::new(OpenOptions::new().append(true).open(path)?)),
+            },
+            manifest,
+        ))
     }
 
     pub fn add_record(
@@ -68,6 +70,7 @@ impl Manifest {
     pub fn add_record_when_init(&self, record: ManifestRecord) -> Result<()> {
         let mut file = self.file.lock();
         file.write_all(serde_json::to_vec(&record)?.as_slice())?;
+        file.sync_all()?;
         Ok(())
     }
 }
